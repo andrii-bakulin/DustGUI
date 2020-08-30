b9cfd59905b9fc617dc05126f0a681580dadda5b
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-08-30 21:24:26 +0700

    Improve DustGUI.Field methods

--- Assets/DustGUI/Scripts/DustGUI-Fields.cs
+++ Assets/DustGUI/Scripts/DustGUI-Fields.cs
@@ -6,39 +6,67 @@ namespace DustEngine
 {
     public static partial class DustGUI
     {
-        public static bool Field(string label, bool value)
+        public static bool Field(string label, bool value, float width = 0f, float height = 0f)
         {
-            return EditorGUILayout.Toggle(label, value);
+            return EditorGUILayout.Toggle(label, value, PackOptions(width, height));
         }
 
-        public static int Field(string label, int value)
+        public static bool Field(string label, bool value, float width, float height, GUIStyle style)
         {
-            return EditorGUILayout.IntField(label, value);
+            return EditorGUILayout.Toggle(label, value, style, PackOptions(width, height));
         }
 
-        public static float Field(string label, float value)
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static int Field(string label, int value, float width = 0f, float height = 0f)
         {
-            return EditorGUILayout.FloatField(label, value);
+            return EditorGUILayout.IntField(label, value, PackOptions(width, height));
         }
 
-        public static string Field(string label, string value)
+        public static int Field(string label, int value, float width, float height, GUIStyle style)
         {
-            return EditorGUILayout.TextField(label, value);
+            return EditorGUILayout.IntField(label, value, style, PackOptions(width, height));
         }
 
-        public static Vector3 Field(string label, Vector3 value)
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static float Field(string label, float value, float width = 0f, float height = 0f)
         {
-            return EditorGUILayout.Vector3Field(label, value);
+            return EditorGUILayout.FloatField(label, value, PackOptions(width, height));
         }
 
-        public static Color Field(string label, Color value)
+        public static float Field(string label, float value, float width, float height, GUIStyle style)
         {
-            return EditorGUILayout.ColorField(label, value);
+            return EditorGUILayout.FloatField(label, value, style, PackOptions(width, height));
         }
 
-        public static AnimationCurve Field(string label, AnimationCurve value)
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static string Field(string label, string value, float width = 0f, float height = 0f)
         {
-            return EditorGUILayout.CurveField(label, value);
+            return EditorGUILayout.TextField(label, value, PackOptions(width, height));
+        }
+
+        public static string Field(string label, string value, float width, float height, GUIStyle style)
+        {
+            return EditorGUILayout.TextField(label, value, style, PackOptions(width, height));
+        }
+
+        //--------------------------------------------------------------------------------------------------------------
+
+        public static Vector3 Field(string label, Vector3 value, float width = 0f, float height = 0f)
+        {
+            return EditorGUILayout.Vector3Field(label, value, PackOptions(width, height));
+        }
+
+        public static Color Field(string label, Color value, float width = 0f, float height = 0f)
+        {
+            return EditorGUILayout.ColorField(label, value, PackOptions(width, height));
+        }
+
+        public static AnimationCurve Field(string label, AnimationCurve value, float width = 0f, float height = 0f)
+        {
+            return EditorGUILayout.CurveField(label, value, PackOptions(width, height));
         }
 
         public static AnimationCurve Field(string label, AnimationCurve value, float width, float height, Color color, Rect ranges = new Rect())
