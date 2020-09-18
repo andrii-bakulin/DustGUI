0b6d86b91d4b8e1228997f900fe13b1a5bac1b1b
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-09-18 15:46:21 +0700

    Improve code quality: replace const to static readonly

--- Assets/DustGUI/Scripts/Editor/Config.cs
+++ Assets/DustGUI/Scripts/Editor/Config.cs
@@ -7,16 +7,16 @@ namespace DustEngine
     {
         public static class Config
         {
-            public const int ICON_BUTTON_WIDTH = 28;
-            public const int ICON_BUTTON_HEIGHT = 28;
-            public const int ICON_BUTTON_PADDING = 2;
+            public static readonly int ICON_BUTTON_WIDTH = 28;
+            public static readonly int ICON_BUTTON_HEIGHT = 28;
+            public static readonly int ICON_BUTTON_PADDING = 2;
 
             public static readonly Color BUTTON_PRESSED_COLOR = new Color(0.75f, 0.75f, 0.75f);
 
-            public const string RESOURCE_ICON_ARROW_DOWN = "DustGUI/Arrow-Down";
-            public const string RESOURCE_ICON_ARROW_LEFT = "DustGUI/Arrow-Left";
-            public const string RESOURCE_ICON_ARROW_RIGHT = "DustGUI/Arrow-Right";
-            public const string RESOURCE_ICON_ARROW_UP = "DustGUI/Arrow-Up";
+            public static readonly string RESOURCE_ICON_ARROW_DOWN = "DustGUI/Arrow-Down";
+            public static readonly string RESOURCE_ICON_ARROW_LEFT = "DustGUI/Arrow-Left";
+            public static readonly string RESOURCE_ICON_ARROW_RIGHT = "DustGUI/Arrow-Right";
+            public static readonly string RESOURCE_ICON_ARROW_UP = "DustGUI/Arrow-Up";
         }
     }
 }
